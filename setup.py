ebd58979ed7623bbd1f05704f47437a5ee5bf07e
PaulFilms <pablopila.spain@gmail.com>
2023-12-11 19:52:03 +0100

    Changes to be committed: 	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+'''
+Build Script for Cython Files
+\n
+`ATTENTION:`
+This code is incomplete, all functions have to be rewritten in.pyx files
+\n
+`cmd:` 
+python setip.py build_ext --inplace
+'''
+
+__update__ = '2023.12.02'
+__author__ = 'PABLO GONZALEZ PILA <pablogonzalezpila@gmail.com>'
+
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+ext_modules = [
+    Extension(
+        "pydeveloptools",
+        sources=[
+            # "func_database.pyx"
+            ],
+        # Aquí puedes añadir más opciones como librerías externas, directorios, etc.
+    )
+]
+
+setup(
+    name='pydeveloptools',
+    ext_modules=cythonize(ext_modules),
+)
+
