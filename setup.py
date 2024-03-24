8158fc7a50ceab79781630a08c1c8ea4c08408cf
PaulFilms <pablopila.spain@gmail.com>
2024-03-24 22:30:40 +0100

    Changes to be committed: 	modified:   MANIFEST.in 	new file:   pydeveloptools/__init__.py 	renamed:    database/ms_access.py -> pydeveloptools/database/ms_access.py 	renamed:    database/seatable.py -> pydeveloptools/database/seatable.py 	renamed:    database/sqlite.py -> pydeveloptools/database/sqlite.py 	renamed:    forms/PYQT_QACQUISITIONS.ui -> pydeveloptools/forms/PYQT_QACQUISITIONS.ui 	renamed:    forms/PYQT_QACQUISITIONS_ui.py -> pydeveloptools/forms/PYQT_QACQUISITIONS_ui.py 	renamed:    forms/PYQT_QCALENDAR.ui -> pydeveloptools/forms/PYQT_QCALENDAR.ui 	renamed:    forms/PYQT_QLICENSE.ui -> pydeveloptools/forms/PYQT_QLICENSE.ui 	renamed:    forms/PYQT_QLICENSE_ui.py -> pydeveloptools/forms/PYQT_QLICENSE_ui.py 	renamed:    forms/PYQT_QLIST.ui -> pydeveloptools/forms/PYQT_QLIST.ui 	renamed:    forms/PYQT_QLIST_FORM.py -> pydeveloptools/forms/PYQT_QLIST_FORM.py 	renamed:    forms/PYQT_QLIST_FORM.ui -> pydeveloptools/forms/PYQT_QLIST_FORM.ui 	renamed:    forms/PYQT_QLIST_FORM_ui.py -> pydeveloptools/forms/PYQT_QLIST_FORM_ui.py 	renamed:    forms/PYQT_QLIST_ui.py -> pydeveloptools/forms/PYQT_QLIST_ui.py 	renamed:    forms/PYQT_QMARKDOWN.ui -> pydeveloptools/forms/PYQT_QMARKDOWN.ui 	renamed:    forms/PYQT_QMARKDOWN_ui.py -> pydeveloptools/forms/PYQT_QMARKDOWN_ui.py 	renamed:    forms/PYQT_QTABLE_DICT.ui -> pydeveloptools/forms/PYQT_QTABLE_DICT.ui 	renamed:    forms/PYQT_QTABLE_FORM.py -> pydeveloptools/forms/PYQT_QTABLE_FORM.py 	renamed:    forms/PYQT_QTABLE_FORM.ui -> pydeveloptools/forms/PYQT_QTABLE_FORM.ui 	renamed:    forms/info.ico -> pydeveloptools/forms/info.ico 	renamed:    func_database.py -> pydeveloptools/func_database.py 	renamed:    func_license.py -> pydeveloptools/func_license.py 	renamed:    func_pdf.py -> pydeveloptools/func_pdf.py 	renamed:    func_pyqt6.py -> pydeveloptools/func_pyqt6.py 	renamed:    func_system.py -> pydeveloptools/func_system.py 	renamed:    func_xlsx.py -> pydeveloptools/func_xlsx.py 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -10,12 +10,36 @@ cmd:
     python setip.py build_ext --inplace
 '''
 
-__update__ = '2023.12.02'
-__author__ = 'PABLO GONZALEZ PILA <pablogonzalezpila@gmail.com>'
+__update__ = '2023.03.24'
+__author__ = 'PABLO PILA'
+__author_email__ = "pablogonzalezpila@gmail.com"
 
-from setuptools import setup, Extension
+from setuptools import setup, find_packages, Extension
 # from Cython.Build import cythonize
 
+setup(
+    name = "pydeveloptools",
+    # packages = find_packages(), # con find_pachages no conseguir hacerlo funcionar
+    packages=["pydeveloptools"],
+    include_package_data=True, # muy importante para que se incluyan archivos sin extension .py
+    package_data={'pydeveloptools': ['database/*.py', 'forms/*']}, 
+    version = __update__,
+    author = __author__,
+    author_email = __author_email__,
+    url = "https://github.com/PaulFilms/pydeveloptools.git",
+    long_description = "README.md",
+    # license = "www.unlicense.org",
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        ],
+)
+
+
+''' CONFIG DE CPYTHON
 ext_modules = [
     Extension(
         "pydeveloptools",
@@ -31,3 +55,4 @@ setup(
     # ext_modules=cythonize(ext_modules),
 )
 
+'''
